6b482f335f205787a0a2c6a66a3eedf4607880a6
Aandnota <myers.alex@gmail.com>
2016-07-25 18:24:38 -0500

    Created

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top}, 500);
+	    return false;
+	});
+
+	$('.menu-toggle').click(function() {
+		$('.menu-toggle').toggleClass('menu-open-close'),
+		$('.nav-links-fac').toggleClass('menu-show-hide');
+	});
+
+	$(window).scroll(function() {
+		if($(document).scrollTop() > 50) {
+			$('.nav-span').css({
+				'background-color': '#4C4C4C',
+				'background-image': 'url(imgs/ccas-seal.png)',
+			});
+		} else {
+			$('.nav-span').css({
+				'background-color': 'transparent',
+				'background-image': 'none'
+			});;
+		}
+	});
+
+	$(".toggle").click(function() {
+		$("#x-alu").toggle('500');
+	});
+
+
+	$(".toggle").click(function() {
+		$("#x-3d").toggle('500');
+	});
+
+});
